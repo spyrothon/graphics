fdc9ad30f3b17aa6abd3f210ca71f4ea59259632
faultyserver <jonegeland@gmail.com>
2022-09-26 23:33:49 -0400

    remount admin routes as the base path

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -6,7 +6,6 @@ interface ImportMetaEnv {
   readonly SPYROTHON_SYNC_HOST: string;
   readonly SPYROTHON_ASSETS_ENDPOINT: string;
   readonly SPYROTHON_APP_HOST: string;
-  readonly SPYROTHON_ADMIN_PATH: string;
 }
 
 interface ImportMeta {
