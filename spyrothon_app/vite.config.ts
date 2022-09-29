312c94b47abf8a6d47a9c6175e011c46115098c8
faultyserver <jonegeland@gmail.com>
2022-09-29 10:58:39 -0400

    fix vite references for moved projects

--- spyrothon_app/vite.config.ts
+++ spyrothon_app/vite.config.ts
@@ -6,14 +6,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
-    postcss: resolve(__dirname, "../../tools/postcss"),
+    postcss: resolve(__dirname, "../tools/postcss"),
   },
   resolve: {
     alias: {
       "@app": resolve(__dirname, "src"),
     },
   },
-  envDir: "../../env",
+  envDir: "../env",
   envPrefix: "SPYROTHON_",
   plugins: [react()],
 });
